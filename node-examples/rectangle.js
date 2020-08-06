73bd58472e20584f6391629d2fd0d4b3667f58ae
Prudence Codjia <pcodjia@Prudences-MBP.fios-router.home>
2020-08-06 00:57:49 -0400

    Node HTTP Example 2

--- node-examples/rectangle.js
+++ node-examples/rectangle.js
@@ -1,6 +1,3 @@
-const rect = require('./rectangle');
-
-
 function solveRect(l, w) {
     console.log(`Solving for rectangle with dimensions: ${l}, ${w}`);
 
@@ -15,7 +12,6 @@ function solveRect(l, w) {
     console.log('This statement is logged after the call to rect()');
 }
 
-solveRect(2, 4);
-solveRect(3, 5);
-solveRect(0, 5);
-solveRect(-3, 5);
+
+
+
