d4b73f2811ee63b6b161489dc0d5ce84de1d341f
Prudence Codjia <pcodjia@Prudences-MBP.fios-router.home>
2020-08-19 20:09:59 -0400

    Mongoose Part3

--- node-mongoose/models/index.js
+++ node-mongoose/models/index.js
@@ -1,5 +1,39 @@
 const mongoose = require('mongoose');
-const Campsite = require('./models/campsite');
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+const campsiteSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    comments: [commentSchema]
+}, {
+    timestamps: true
+});
 
 const url = 'mongodb://localhost:27017/nucampsite';
 const connect = mongoose.connect(url, {
